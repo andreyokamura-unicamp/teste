d63bcbe497ceb1f6afbf27f945a78d5b05497208
andreyokamura-unicamp <163178547+andreyokamura-unicamp@users.noreply.github.com>
2024-10-30 17:15:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 #from fileinput import filename 
 from flask import Flask
 
-
 app = Flask(__name__)
-from app.main import *
+#from app.main import *
 
 if __name__ == '__main__':
     app.run()
+    
+@app.route('/')
+def homepage():
+    return teste
