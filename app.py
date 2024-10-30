45b35ff5cb3271c94806b73d934fed11f98c57ee
andreyokamura-unicamp <163178547+andreyokamura-unicamp@users.noreply.github.com>
2024-10-30 20:08:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 #from app.main import *
 
-if __name__ == '__main__':
-    app.run()
     
 @app.route('/')
 def homepage():
     return "teste"
+
+if __name__ == '__main__':
+    app.run()
+
